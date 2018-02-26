bf7535f7fd887e8251b3042f27d6f9ce4c5d5f9c
Lamhot Simamora <lamhotsimamora36@gmail.com>
2018-02-26 10:37:14 +0700

    lupa au !

--- js/component.js
+++ js/component.js
@@ -7,7 +7,7 @@ $app.setContent('<br><br><center id="view_button"><button class="btn btn-warning
 					+'<div class="input-group mb-3"> <div class="input-group-prepend">'
 					+'<button class="btn btn-outline-success" id="btn_url" type="button">Open</button></div>'
 					+'<select class="form-control" id="display_menu"> </select> </div>'
-					+'<span class="badge badge-warning">Press Backspace for Preious</span>&nbsp<span class="badge badge-primary">Press Enter for Next</span></br>'
+					+'<span class="badge badge-warning">Press Backspace for Previous</span>&nbsp<span class="badge badge-primary">Press Enter for Next</span></br>'
 					+'<img src="" width="600" height="600" id="albums" class="img-thumbnail"></center> <br><br>');
 
 $menu = __({
